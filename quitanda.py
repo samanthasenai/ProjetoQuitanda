8570fa3af7b226a34da583c6ee6239460e961e2b
samanthasenai <samantha.almeida.senai@gmail.com>
2023-11-10 13:01:10 -0300

    update README.md

--- quitanda.py
+++ quitanda.py
@@ -39,6 +39,19 @@ def index():
     title = "Home"
     return render_template("home.html", produtos=produtos, title=title)
 
+# ROTA DA PÁGINA ACESSO
+@app.route("/acesso", methods=['post'])
+def acesso():
+    global usuario, senha
+    usuario_informado = request.form["usuario"]
+    senha_informada = request.form["senha"]
+    if usuario == usuario_informado and senha == senha_informada:
+        session["login"] = True
+        return redirect('/adm')
+    else:
+        return render_template("login.html",msg="Usuário/Senha estão incorretos!")
+
+
 #ROTA DA PÁGINA LOGIN
 @app.route("/login")
 def login():
